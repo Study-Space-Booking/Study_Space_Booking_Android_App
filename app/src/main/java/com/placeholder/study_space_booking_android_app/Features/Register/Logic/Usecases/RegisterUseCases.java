6e798397058944d7e67ecfc57ebe038da40a7321
hcy <2320032466@qq.com>
2020-06-22 12:21:27 +0800

    the commit

--- app/src/main/java/com/placeholder/study_space_booking_android_app/Features/Register/Logic/Usecases/RegisterUseCases.java
+++ app/src/main/java/com/placeholder/study_space_booking_android_app/Features/Register/Logic/Usecases/RegisterUseCases.java
@@ -1,6 +1,8 @@
 package com.placeholder.study_space_booking_android_app.Features.Register.Logic.Usecases;
 
 
+import android.content.Intent;
+
 import com.placeholder.study_space_booking_android_app.Core.Beans.NormalUser;
 import com.placeholder.study_space_booking_android_app.Core.Beans.Result;
 import com.placeholder.study_space_booking_android_app.Features.Register.Data.Repository.RegisterRepositoryImplementation;
@@ -11,6 +13,9 @@ import com.placeholder.study_space_booking_android_app.Features.Register.Logic.R
 public class RegisterUseCases {
     private final RegisterRepository registerRepository;
     private static volatile RegisterUseCases instance;
+    private static final int PASSWORD_LENGTH = 6;
+    private static final int NUMBER_OF_CHARACTERS_NEEDED = 1;
+    private static final int NUMBER_OF_DIGITS_NEEDED = 1;
 
     private RegisterUseCases(RegisterRepository registerRepository) {
         this.registerRepository = registerRepository;
@@ -25,9 +30,16 @@ public class RegisterUseCases {
 
     public Result<NormalUser> validate(String userName, String password, String confirmPassword, RegisterListener registerListener) {
         if(userName.equals("") || password.equals("") || confirmPassword.equals("")) {
-            return new Result.Handle(new IllegalArgumentException("Check user information"));
+            return new Result.Handle(new IllegalArgumentException("All the fields need to be completed"));
         } else if(!passwordMatch(password, confirmPassword)) {
-            return new Result.Handle(new IllegalArgumentException("Confirm password"));
+            return new Result.Handle(new IllegalArgumentException("Confirm password needs to match the passowrd entered"));
+        } else if(!isValidUserName(userName)) {
+            return new Result.Handle(new IllegalArgumentException("Register with the school email address"));
+        } else if(!isPasswordLengthValid(password)) {
+            return new Result.Handle(new IllegalArgumentException("Password needs to have a length of at least " + PASSWORD_LENGTH));
+        } else if(!isPasswordValid(password)) {
+            return new Result.Handle(new IllegalArgumentException(
+                    "Password needs to have at least " + NUMBER_OF_CHARACTERS_NEEDED + " character and " + NUMBER_OF_DIGITS_NEEDED + " digit"));
         } else {
             registerRepository.hasExistingUser(userName, registerListener);
             return new Result.Accepted<>(null);
@@ -40,5 +52,38 @@ public class RegisterUseCases {
 
     public boolean passwordMatch(String password, String confirmPassword) {
         return password.equals(confirmPassword);
+    }
+
+    public boolean isValidUserName(String userName) {
+        if(userName.length() != 18) {
+            return false;
+        } else {
+            boolean check = true;
+            for(int i = 0; i < 7; i = i + 1) {
+                if(userName.charAt(i + 1) < '0' || userName.charAt(i + 1) > '9') {
+                    check = false;
+                }
+            }
+            return check && userName.charAt(0) == 'e' && userName.substring(8, 18).equals("@u.nus.edu");
+        }
+    }
+
+    public boolean isPasswordLengthValid(String password) {
+        return password.length() >= 6;
+    }
+
+    public boolean isPasswordValid(String password) {
+        boolean hasCharacter = false;
+        boolean hasDigit = false;
+        for(int i = 0; i < password.length(); i = i + 1) {
+            if(Character.isLetter(password.charAt(i))) {
+                hasCharacter = true;
+            } else if (Character.isDigit(password.charAt(i))) {
+                hasDigit = true;
+            } else {
+
+            }
+        }
+        return hasCharacter && hasDigit;
     }
 }
